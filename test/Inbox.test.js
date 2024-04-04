d34ea1ce3525817918aa355e06f0388e0546a940
aarushsing <117664877+aarushsing@users.noreply.github.com>
2024-04-04 22:11:32 +0530

    Add files via upload

---
+++ test/Inbox.test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+
+let accounts;
+let inbox;
+ beforeEach(async () => {
+
+    // get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    
+
+    // use one of those acounts to delppt the contract.
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode , arguments: ['Hi there!']})
+      .send ({ from: accounts[0], gas: '1000000'});
+});                                                            //0x00bA7acaa8aDdFdf014315A6b81B963De1F46d58
+
+describe('Inbox', ()=>{
+    it('deploys a contract' ,  ()=>{
+        assert.ok(inbox.options.address);
+    });
+
+    it('has some default message', async ()=> {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+    it("can change the message", async ()=>{
+        await inbox.methods.setMessage('changes are done in message').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'changes are done in message');
+    });
+});
+
